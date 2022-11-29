100aab5f9612d0968ed3bf2f3260245cbefe3629
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2022-11-29 15:38:10 -0300

    update mutafoca

--- chatClient.py
+++ chatClient.py
@@ -1,19 +1,25 @@
 import socket
 
-HOST = input("Insira o ip do servidor ao qual quer se conectar\n")
-PORT = input("Insira a porta do servidor ao qual quer se conectar\n")
+HOST = "172.16.1.4"#input("Insira o ip do servidor ao qual quer se conectar\n")
+PORT = 8123 #input("Insira a porta do servidor ao qual quer se conectar\n")
 SERVER_NAME = ""
 addr = (HOST, int(PORT))
+nick = "Vorotex"
 
 aux = 0
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        if aux == 0:
-            aux = 1
-            SERVER_NAME = s.recv(64)
-            print(f"Conectado ao servidor {SERVER_NAME}")
-        message = input("Escreve aqui: ")
-        s.sendto(bytes(message, 'utf-8'), addr)
-        if message == '#sair':
-            break
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+	while True:    
+		if aux == 0:
+		    aux = 1
+		    s.sendto(bytes(nick, 'utf-8'), addr)
+		    x = s.recvfrom(64)
+		    SERVER_NAME = x[0].decode('utf-8')
+		    print(f"Conectado ao servidor {SERVER_NAME}")
+		message = input("Escreve aqui: ")
+		s.sendto(bytes(message, 'utf-8'), addr)
+		if message == '#sair':
+		    break
+		msg = s.recvfrom(1024)
+		print(msg[0].decode('utf-8'))
