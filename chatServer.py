31275738155b325d1185e28773b20568cd0893b9
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2022-11-30 13:54:12 -0300

    Mudança, agora tá certo

--- chatServer.py
+++ chatServer.py
@@ -1,46 +1,61 @@
 import socket
-import threading
+from threading import Thread, Lock
 import time
-from multiprocessing import shared_memory
 
-shm_a = shared_memory.SharedMemory(create=True, size=1024)
-listaAddr = shm_a
+lock = Lock()
 
-HOSTNAME = socket.gethostname()
-#HOSTIP = socket.gethostbyname(HOSTNAME)
-HOSTIP = "127.0.0.1"
+class Th(Thread):
+
+    def __init__(self, mode):
+        Thread.__init__(self)
+        global nickname
+        self.nickname = nickname
+        global s
+        self.sock = s
+        self.mode = mode
+
+    def run(self):
+        global listaAddr
+        global listaTime
+        if self.mode == 'r':
+            while True:
+                (data, addr) = self.sock.recv(500)
+                msgPessoa = data.decode('utf-8')
+                print(msgPessoa)
+                lock.acquire()
+                if addr not in listaAddr:
+                    nickPessoa = msgPessoa[0:msgPessoa.index(":")]
+                    listaAddr[addr] = nickPessoa
+                listaTime[addr] = time.time()
+                lock.release()
+        elif self.mode == 'e':
+            while True:
+                msg = input("Escreve sua msg:")
+                if msg == "#list":
+                    lock.acquire()
+                    for endereco in listaTime.keys():
+                        if listaTime[endereco] - time.time() > 60:
+                            del listaAddr[endereco]
+                            del listaTime[endereco]
+                        print(listaAddr[endereco])
+                    lock.release()
+                else:
+                    enviado = self.nickname + ":" + msg
+                    self.sock.sendto(bytes(enviado, 'utf-8'), ("224.0.0.200", 2020))
+
+HOSTIP = "172.16.1.4"
 PORTA = 8123 # The port used by the server
 addr = (HOSTIP, PORTA)
 
-def counter(listaTime, listaAddr):
-    while True:
-        for el in listaTime:
-            if time.time() - el.value:
-                print(f"{listaAddr[el.key]} saiu por inatividade.")
-                listaTime.pop(el.key)
-                listaAddr.pop(el.key)
-
-print("Inicializando Servidor")
-print(f"{HOSTNAME} hosteando {HOSTIP}:{PORTA}")
-print("Aguardando conexões\n")
+nickname = input("Insere teu nickname:")
 
 listaAddr = {}
 listaTime = {}
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    # Bind the socket to the port
-    server_address = addr
-    s.bind(server_address)
-    while True:
-        (data, addr) = s.recv(1024)
-        if addr not in listaAddr:
-            s.send(b"Varzinha")
-            print(f"{data} entrou no chat!\n")
-            listaAddr[addr] = data
-        else:
-            nickname = listaAddr[addr]
-            if data == "#sair":
-                break
-            print(f"{nickname}:{data}")
-            s.sendall(data)
-        listaTime[addr] = time.time()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(addr)
+
+thEnvia = Th('e')
+thRecebe = Th('r')
+thEnvia.start()
+thRecebe.start()
